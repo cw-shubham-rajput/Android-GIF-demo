c3b29031ce5fa084e68da8e294f6431b5c379269
Shubham Rajput <srrajput042@gmail.com>
2017-02-11 21:08:36 +0530

    files uploaded

---
+++ src/main/java/com/example/shubham/gif_demo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.shubham.gif_demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget(imageView);
+        Glide.with(this).load(R.drawable.giphy).into(imageViewTarget);
+    }
+}
